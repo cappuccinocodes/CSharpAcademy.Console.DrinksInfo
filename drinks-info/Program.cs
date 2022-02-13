84df26fa3f08ae5b4be169b2f02779050a29da94
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2022-02-14 09:17:33 +1000

    initial commit

---
+++ drinks-info/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace drinks_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInput userInput = new();
+            userInput.GetCategoriesInput();
+        }
+    }
+}
